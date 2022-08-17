e83fb67505be6c1466fc95b59ce4040beed14272
J-bott <88146840+J-bott@users.noreply.github.com>
2022-08-17 18:47:25 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("seta");
 const menu = document.getElementById("menu");
 const vetores = document.getElementsByClassName("vetor");
 const closeVector = document.getElementById("close-vector");
+const salas = document.getElementById("salas");
 
 btn.onclick = function () {
   //levanta ou desce menu flutuante
@@ -13,11 +14,60 @@ btn.onclick = function () {
   }
 }
 
+function openSalas() {
+  //desabilita botoes do menu flutuante, abilita botoes de salas
+
+  let btns = document.getElementsByClassName("btn");
+  let btnSala = document.getElementsByClassName ("btn-sala");
+
+  for(i=0; i < btns.length; i++) {
+    btns[i].style.display = "none";
+  }
+  for(i=0; i < btnSala.length; i++) {
+      btnSala[i].style.display = "block";
+    }
+
+}
+function closeSalas() {
+  //reverte openSalas() e openOficinas()
+
+  let btns = document.getElementsByClassName("btn");
+  let btnSala = document.getElementsByClassName ("btn-sala");
+  let btnOficina = document.getElementsByClassName ("btn-oficina");
+
+  for(i=0; i < btns.length; i++) {
+    btns[i].style.display = "block";
+  }
+  for(i=0; i < btnSala.length; i++) {
+      btnSala[i].style.display = "none";
+    }
+    for(i=0; i < btnOficina.length; i++) {
+      btnOficina[i].style.display = "none";
+    }
+
+
+}
+
+function openOficinas(){
+  //desabilita botoes do menu flutuante, abilita botoes da oficina
+
+  let btns = document.getElementsByClassName("btn");
+  let btnOficina = document.getElementsByClassName ("btn-oficina");
+
+  for(i=0; i < btns.length; i++) {
+    btns[i].style.display = "none";
+  }
+  for(i=0; i < btnOficina.length; i++) {
+      btnOficina[i].style.display = "block";
+    }
+
+}
 closeVector.onclick = function () {
   //botao esc (reabilita menu flutuante, fecha todos caminhos);
 
     menu.style.display = "flex";
     btn.onclick();
+    closeSalas();
     closeVector.style.display = "none";
     for (i = 0; i < vetores.length; i++) {
         vetores[i].style.display = "none";
@@ -86,4 +136,29 @@ function VecAuditorio() {
   disableMenu();
 }
 
+function VecMadeiras() {
+  //abre caminho x, dispara animação, desabilita menu flutuante, abilita botao esc;
+
+  var x = document.getElementById("Vec-madeiras");
+  x.style.display = "block";
+  x.style.animation = "dash 2s linear forwards";
+  disableMenu();
+}
+
+function VecTipografia() {
+  //abre caminho x, dispara animação, desabilita menu flutuante, abilita botao esc;
+
+  var x = document.getElementById("Vec-tipografia");
+  x.style.display = "block";
+  x.style.animation = "dash 2s linear forwards";
+  disableMenu();
+}
+function VecFotografia() {
+  //abre caminho x, dispara animação, desabilita menu flutuante, abilita botao esc;
+
+  var x = document.getElementById("Vec-fotografia");
+  x.style.display = "block";
+  x.style.animation = "dash 2s linear forwards";
+  disableMenu();
+}
 
